38125da9436b7cf187184e81c6adc0c0462421cd
Artem <artemborisencko@gmail.com>
2025-01-08 18:48:22 +0200

    five iteration

---
+++ main.js
@@ -0,0 +1,20 @@
+function saveDataFunction() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const userData = [];
+
+  userData.push(email);
+  userData.push(password);
+
+  const userDataString = JSON.stringify(userData);
+
+  const file = new Blob([userDataString], {type: "text/plain" });
+
+  const anchor = document.createElement("a");
+  anchor.href = URL.createObjectURL(file);
+  anchor.download = "userData.txt";
+  anchor.click();
+
+  URL.revokeObjectURL(anchor.href);
+};
